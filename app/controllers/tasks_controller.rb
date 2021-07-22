883719561adcffb8c8033cccf9ae5b60d3399c05
Francesca Ahumada <javiera.francesca@gmail.com>
2021-07-21 21:58:38 -0400

    rails task manager

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,20 @@
 class TaskController < ApplicationController
+  def index
+    @tasks = task.all
+  end
+
+  def show
+    @id = params[:id]
+    @record = Task.find(@id)
+  end
+
+  def create; end
+
+  def new; end
+
+  def update; end
+
+  def edit; end
+
+  def destroy; end
 end
